0647fbf0091b7615987c7fe2578e4a6a52bcba04
Mohammad Taslimi <mcinext.work@gmail.com>
2025-03-05 21:05:01 +0330

    deleted: test_local_models.py

--- src/tejarat_rag/main.py
+++ src/tejarat_rag/main.py
@@ -1,8 +1,8 @@
 from .dspy_prompt import RAGHandler
 
 if __name__ == "__main__":
-    rag_pipeline = RAGHandler()
     
+    rag_pipeline = RAGHandler(is_local_llm=False)
     user_query = "یکم درمورد بانک تجارت توضیح میدی"
     answer = rag_pipeline.query(question=user_query)
     print(answer)
